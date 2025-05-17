d9e54171e760a1592bca598b2d462df8af2b9202
Hosung99 <shson1217@kw.ac.kr>
2025-05-17 13:50:56 +0900

    chore: 초기 세팅 완료

--- App.tsx
+++ App.tsx
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   ScrollView,
@@ -23,6 +15,8 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import './global.css';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -61,15 +55,6 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
   const safePadding = '5%';
 
   return (
@@ -78,10 +63,9 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        style={backgroundStyle}>
+      <ScrollView style={backgroundStyle}>
         <View style={{paddingRight: safePadding}}>
-          <Header/>
+          <Header />
         </View>
         <View
           style={{
@@ -90,8 +74,10 @@ function App(): React.JSX.Element {
             paddingBottom: safePadding,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            <Text className="text-red-500" style={styles.highlight}>
+              App.tsx
+            </Text>{' '}
+            to change this screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,19 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {withNativeWind} = require('nativewind/metro');
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
+ * this is js bundler
  */
-const config = {};
+const config = getDefaultConfig(__dirname);
+const mergedConfig = mergeConfig(config, {
+  resolver: {
+    sourceExts: ['js', 'jsx', 'ts', 'tsx', 'json', 'mjs'],
+  },
+});
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// NativeWind 설정
+module.exports = withNativeWind(mergedConfig, {input: './global.css'});
