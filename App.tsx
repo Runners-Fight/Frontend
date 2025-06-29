24a6de1eb0996c0b474e5d6ea08b6a2c79b0e228
Hosung99 <shson1217@kw.ac.kr>
2025-06-30 01:43:35 +0900

    refactor: AppNavigation 분리

--- App.tsx
+++ App.tsx
@@ -1,25 +1,7 @@
-import OnBoardingScreen from '@screens/OnBoarding';
-import HomeScreen from '@screens/Home';
-import MyScreen from '@screens/My';
-import AlertScreen from '@screens/Alert';
-import CrewScreen from '@screens/Crew';
-import {createStaticNavigation} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AppNavigation from '@navigations/AppNavigation';
 import './global.css';
 
-const RootStack = createNativeStackNavigator({
-  screens: {
-    OnBoarding: OnBoardingScreen,
-    Home: HomeScreen,
-    My: MyScreen,
-    Alert: AlertScreen,
-    Crew: CrewScreen,
-  },
-});
-
-const Navigation = createStaticNavigation(RootStack);
-
 function App(): React.JSX.Element {
-  return <Navigation />;
+  return <AppNavigation />;
 }
 export default App;
--- babel.config.js
+++ babel.config.js
@@ -22,6 +22,7 @@ module.exports = {
           '~': './src',
           '@components': './src/components',
           '@screens': './src/screens',
+          '@navigations': './src/navigations',
         },
       },
     ],
