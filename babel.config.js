3c9ddc7aaff8ce8dd74460194fada8c4e22f465a
Hosung99 <shson1217@kw.ac.kr>
2025-06-30 01:34:23 +0900

    chore: 절대경로 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,27 @@ module.exports = {
   plugins: [
     '@babel/plugin-transform-class-static-block',
     'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '~': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+        },
+      },
+    ],
   ],
 };
